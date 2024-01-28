abba01a066a94139d185a81a3016cdf64619a495
Alex Vo <alexanvo@tamu.edu>
2024-01-28 08:41:19 -0600

    increasing font sizes

--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -2,7 +2,7 @@ const Message = ({ message, sender }) => {
   return (
     <div id="messageWrapper">
       <div id="contentWrapper">
-        <div style={{fontWeight: "bold  "}}>{sender}</div>
+        <div style={{fontWeight: "bold  ", fontSize: "20px"}}>{sender}</div>
         <div id="indMessage">{message}</div>
       </div>
     </div>
